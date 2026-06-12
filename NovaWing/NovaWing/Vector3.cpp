dfc93aca158e236eeac6965d3db7aa3fecc7c7f3
ko1307 <208754810+13ko13@users.noreply.github.com>
2026-06-13 00:51:35 +0900

    Vector3クラスを追加

---
+++ NovaWing/NovaWing/Vector3.cpp
@@ -0,0 +1,123 @@
+﻿#include "Vector3.h"
+#include <cmath>
+
+Vector3::Vector3():
+	m_x(0.0f),m_y(0.0f),m_z(0.0f)
+{
+
+}
+
+
+Vector3::Vector3(float x, float y, float z):
+	m_x(x),m_y(y),m_z(z)
+{
+}
+
+/// <summary>
+/// ベクトルの大きさを計算する
+/// </summary>
+/// <returns>ベクトルの大きさ</returns>
+float Vector3::Length() const
+{
+	return std::hypot(m_x, m_y, m_z);
+}
+
+void Vector3::Normalize()
+{
+	auto len = Length();
+	if (len == 0.0f)
+	{
+		//0除算を避ける
+		m_x = 0.0f;
+		m_y = 0.0f;
+		m_z = 0.0f;
+		return;
+	}
+
+	//xとyをzlenで割ることで正規化する
+	m_x /= len;
+	m_y /= len;
+	m_z /= len;
+}
+
+Vector3 Vector3::Normalized() const
+{
+	auto len = Length();
+	if (len == 0.0f)
+	{
+		//0除算を避ける
+		return { 0.0f,0.0f,0.0f };
+	}
+	return { m_x / len, m_y / len,m_z / len };
+}
+
+Vector3 Vector3::operator-() const
+{
+	/// ベクトルを反転したベクトルを返す
+	return { -m_x,-m_y ,-m_z };
+}
+
+void Vector3::operator+=(const Vector3& val)
+{
+	/// ベクトルの加算
+	m_x += val.m_x;
+	m_y += val.m_y;
+	m_z += val.m_z;
+}
+
+void Vector3::operator-=(const Vector3& val)
+{
+	/// ベクトルの減算
+	m_x -= val.m_x;
+	m_y -= val.m_y;
+	m_z -= val.m_z;
+}
+
+void Vector3::operator*=(const float scale)
+{
+	/// ベクトルのスカラー倍
+	m_x *= scale;
+	m_y *= scale;
+	m_z *= scale;
+}
+
+Vector3 Vector3::operator+(const Vector3& val) const
+{
+	/// ベクトルの加算
+	return { m_x + val.m_x, m_y + val.m_y,m_z + val.m_z };
+}
+
+Vector3 Vector3::operator-(const Vector3& val) const
+{
+	/// ベクトルの減算
+	return { m_x - val.m_x, m_y - val.m_y,m_z - val.m_z };
+}
+
+Vector3 Vector3::operator*(float scale) const
+{
+	/// ベクトルのスカラー倍
+	return { m_x * scale, m_y * scale,m_z * scale };
+}
+
+bool Vector3::operator!=(const Vector3& val) const
+{
+	/// ベクトルの不等価比較
+	return (m_x != val.m_x || m_y != val.m_y || m_z != val.m_z);
+}
+
+bool Vector3::operator==(const Vector3& val) const
+{
+	return (m_x == val.m_x && m_y == val.m_y && m_z == val.m_z);
+}
+
+VECTOR Vector3::ToDxLib() const
+{
+	//DxLibのVECTOR型に変換する
+	return VGet(m_x, m_y, m_z);
+}
+
+Vector3 Vector3::Lerp(const Vector3& start, const Vector3& end, float t)
+{
+	//線形補間
+	return start + (end - start) * t;
+}
---
+++ NovaWing/NovaWing/Vector3.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include <DxLib.h>
+
+/// <summary>
+/// 3Dベクトル構造体
+/// </summary>
+struct Vector3
+{
+public:
+	Vector3();
+	Vector3(float x, float y, float z);
+
+	float m_x, m_y, m_z;
+	float Length()const;//ベクトルの大きさ
+	void Normalize();//ベクトルの大きさを1にする
+	Vector3 Normalized() const;///大きさを１にしたベクトルを返す
+	Vector3 operator-()const;///ベクトルを反転したベクトルを返す
+	//演算子オーバーロード
+	void operator+=(const Vector3& val);///加算
+	void operator-=(const Vector3& val);///減算
+	void operator*=(const float scale);///ベクトルをn倍する
+	Vector3 operator+(const Vector3& val)const;///加算
+	Vector3 operator-(const Vector3& val)const;///減算
+	Vector3 operator*(float scale)const;///ベクトルの乗算
+	bool operator!=(const Vector3& val)const;///不等価比較
+	bool operator==(const Vector3& val)const;///等価比較
+	VECTOR ToDxLib() const;//DxLibのVECTOR型に変換する
+
+	//Easing関数
+	//勉強用
+	//なぜstaticを付ける必要があるのか？→staticを付けなかったら、わざわざVector3クラスのインスタンスを作成しないといけないから
+	static Vector3 Lerp(const Vector3& start, const Vector3& end, float t);//線形補間
+};
+
+//別名(座標を表すこともあるため)
+using Position3 = Vector3;
+
