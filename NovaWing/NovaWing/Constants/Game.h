b600dbbe5b8592a1df85c70d85ca05ab6b444b5b
ko1307 <208754810+13ko13@users.noreply.github.com>
2026-06-14 01:15:59 +0900

    ゲーム用の定数クラス作成

--- NovaWing/NovaWing/Constants/Game.h
+++ NovaWing/NovaWing/Constants/Game.h
@@ -3,7 +3,7 @@
 namespace Game
 {
 	//ゲーム全体で使用する定数
-	constexpr int kScreenWidth = 1280;
-	constexpr int kScreenHeight = 720;
-	constexpr int kColorBitNum = 32;
+	constexpr int screen_width = 1280;
+	constexpr int screen_height = 720;
+	constexpr int color_bit_num = 32;
 }
