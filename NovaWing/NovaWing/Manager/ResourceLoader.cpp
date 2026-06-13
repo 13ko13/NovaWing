1a07d74105c04b56f2c95ccfbf109fd5f43ad5a4
ko1307 <208754810+13ko13@users.noreply.github.com>
2026-06-14 01:17:08 +0900

    ResourceLoaderの移植

---
+++ NovaWing/NovaWing/Manager/ResourceLoader.cpp
@@ -0,0 +1,100 @@
+﻿#include "ResourceLoader.h"
+#include <DxLib.h>
+#include <cassert>
+
+namespace
+{
+	
+}
+
+ResourceLoader& ResourceLoader::GetInstance()
+{
+	//staticでインスタンスを宣言してそれを返す
+	static ResourceLoader instance;
+	return instance;
+}
+
+void ResourceLoader::LoadAll()
+{
+	//モデルを読み込んでハンドルを保存する
+	int handle = -1;
+}
+
+void ResourceLoader::ReleaseAll()
+{
+	//すべてのリソースを解放する
+	//モデル
+	for (auto& modelH : m_modelHandles)
+	{
+		MV1DeleteModel(modelH.second);
+	}
+	//グラフィック
+	for (auto& graphH : m_graphicHandles)
+	{
+		DeleteGraph(graphH.second);
+	}
+}
+
+int ResourceLoader::GetModel(ModelID id) const
+{
+	//IDをもとにハンドルを返す
+	auto it = m_modelHandles.find(id);
+	
+	//it != m_modelHandles.end()は、idに対応するハンドルが見つかったかどうかをチェックしている
+	//end()は、マップの最後を指すイテレータで、find()が見つからなかったときに返される
+	if (it != m_modelHandles.end())
+	{
+		return it->second;
+	}
+	else
+	{
+		assert(false && "モデルIDが見つかりません");
+		return -1;
+	}
+}
+
+int ResourceLoader::GetGraphic(GraphicID id) const
+{
+	//IDをもとにハンドルを返す
+	auto it = m_graphicHandles.find(id);
+
+	if(it != m_graphicHandles.end())
+	{
+		return it->second;
+	}
+	else
+	{
+		assert(false && "グラフィックIDが見つかりません");
+		return -1;
+	}
+}
+
+int ResourceLoader::GetEffect(EffectID id) const
+{
+	auto it = m_effectHandles.find(id);
+
+	if(it != m_effectHandles.end())
+	{
+		return it->second;
+	}
+	else
+	{
+		assert(false && "エフェクトIDが見つかりません");
+		return -1;
+	}
+}
+
+int ResourceLoader::GetSound(SoundID id) const
+{
+	auto it = m_soundHandles.find(id);
+
+	if (it != m_soundHandles.end())
+	{
+		return it->second;
+	}
+	else
+	{
+		assert(false && "サウンドIDが見つかりません");
+		return -1;
+	}
+}
---
+++ NovaWing/NovaWing/Manager/ResourceLoader.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+#include <unordered_map>
+
+class ResourceLoader
+{
+public:
+	//モデルの種類
+	enum class ModelID : int
+	{
+		
+	};
+
+	//グラフィックの種類
+	enum class GraphicID : int
+	{
+		
+	};
+
+	//エフェクトの種類
+	enum class EffectID : int
+	{
+		
+	};
+
+	//音の種類
+	enum class SoundID :int
+	{
+		
+	};
+
+public:
+	static ResourceLoader& GetInstance();
+
+	//ロード
+	void LoadAll();
+	//解放
+	void ReleaseAll();
+
+	//取得
+	int GetModel(ModelID id) const;
+	int GetGraphic(GraphicID id) const;
+	int GetEffect(EffectID id) const;
+	int GetSound(SoundID id) const;
+
+private:
+	//=defaultでデフォルトコンストラクタを生成する
+	ResourceLoader() = default;
+	//デストラクタも同様
+	~ResourceLoader() = default;
+
+	//コピーコンストラクタとコピー代入演算子は削除する
+	ResourceLoader(const ResourceLoader&) = delete;
+	ResourceLoader& operator=(const ResourceLoader&) = delete;
+
+private:
+	//IDをいれて直感的にアクセスできるようにするためのマップ
+	std::unordered_map<ModelID, int> m_modelHandles;//モデルのハンドルを保存するマップ
+	std::unordered_map<GraphicID, int> m_graphicHandles;//グラフィックのハンドルを保存するマップ
+	std::unordered_map<EffectID, int> m_effectHandles;//エフェクトのハンドルを保存するマップ
+	std::unordered_map<SoundID, int> m_soundHandles;//サウンドのハンドルを保存するマップ
+};
