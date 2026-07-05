8d8882f01aaac017cd0414818d4151ce69d08cb0
ko1307 <208754810+13ko13@users.noreply.github.com>
2026-07-05 23:12:12 +0900

    タイトルシーン追加

---
+++ NovaWing/NovaWing/Scene/TitleScene.cpp
@@ -0,0 +1,97 @@
+﻿#include "TitleScene.h"
+#include "Manager/InputManager.h"
+#include "SceneController.h"
+#include "GameScene.h"
+#include "Main/Application.h"
+
+TitleScene::TitleScene(SceneController& controller):
+	Scene(controller)
+{
+}
+
+TitleScene::~TitleScene()
+{
+}
+
+void TitleScene::Init()
+{
+}
+
+void TitleScene::Update()
+{
+	//InputManagerのインスタンスを取得
+	InputManager& input = InputManager::GetInstance();
+
+	//下入力で選択肢を下に移動(indexを増やす) 
+	if (input.IsTriggered("down"))
+	{
+		//選択肢の最大数で割った余りを取ることで、
+		//選択肢の範囲内に収める
+		m_selectIndex =
+			static_cast<TitleSelect>(
+				(static_cast<int>(m_selectIndex) + 1) %
+				static_cast<int>(TitleSelect::SelectMax
+					));
+	}
+	//上入力で選択肢を上に移動(indexを減らす)
+	if (input.IsTriggered("up"))
+	{
+		//選択肢の最大数で割った余りを取ることで、
+		//選択肢の範囲内に収める
+		m_selectIndex =
+			static_cast<TitleSelect>(
+				(static_cast<int>(m_selectIndex) - 1 + static_cast<int>(TitleSelect::SelectMax)) %
+				static_cast<int>(TitleSelect::SelectMax)
+				);
+	}
+	//決定入力で選択肢を決定する
+	if (input.IsTriggered("ok"))
+	{
+		switch (m_selectIndex)
+		{
+		case TitleSelect::StartGame:
+		{
+			//ゲームシーンに遷移する
+			m_controller.ChangeScene(
+				std::make_shared<GameScene>(
+					m_controller), 60.0f);
+			break;
+		}
+		case TitleSelect::ExitGame:
+		{
+			//ゲームを終了する
+			DxLib_End();
+			break;
+		}
+		}
+	}
+}
+
+void TitleScene::Draw()
+{
+	//ウィンドウサイズ
+	Size wsize = Application::GetInstance().GetWindowSize();
+	//選択肢の位置
+	/*int x = wsize.m_width / 2;
+	int y = wsize.m_height / 2;*/
+
+	//とりあえず左上に選択肢を表示する
+	//選択中の選択肢に矢印を表示する
+	switch (m_selectIndex)
+	{
+	case TitleSelect::StartGame:
+	{
+		DrawFormatString(0, 15, 0xffffff, L"→");
+		DrawFormatString(15, 15, 0xff0000, L"ゲーム開始");
+		DrawFormatString(15, 30, 0xffffff, L"ゲーム終了");
+		break;
+	}
+	case TitleSelect::ExitGame:
+	{
+		DrawFormatString(0, 30, 0xffffff, L"→");
+		DrawFormatString(15, 15, 0xffffff, L"ゲーム開始");
+		DrawFormatString(15, 30, 0xff0000, L"ゲーム終了");
+		break;
+	}
+	}
+}
---
+++ NovaWing/NovaWing/Scene/TitleScene.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "Scene.h"
+class TitleScene : public Scene
+{
+public:
+	TitleScene(SceneController& controller);
+	~TitleScene();
+
+	void Init() override;//初期化処理
+	void Update() override;//更新処理
+	void Draw() override;//描画処理
+
+private:
+	enum class TitleSelect
+	{
+		StartGame,//ゲーム開始
+		ExitGame,//ゲーム終了
+
+		SelectMax,//選択肢の最大数
+	};
+
+	//現在プレイヤーが選んでいる選択肢
+	TitleSelect m_selectIndex = TitleSelect::StartGame;
+};
+
