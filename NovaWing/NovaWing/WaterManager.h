54eb0d3e11b63d7b05b246d394662b783afa8332
ko1307 <208754810+13ko13@users.noreply.github.com>
2026-07-07 00:13:39 +0900

    海作成準備1

--- NovaWing/NovaWing/WaterManager.h
+++ NovaWing/NovaWing/WaterManager.h
@@ -1,10 +1,33 @@
 ﻿#pragma once
+#include <vector>
+
 class WaterManager
 {
 public:
 	WaterManager();
 	~WaterManager();
 
-	void Init();
+	void Init();//初期化処理
+
+	//頂点データを生成する
+	void CreateVertexData();
+
+private:
+	//フレームカウンター
+	int m_frame = 0;
+
+	//↓なぜunsigned shortなのか
+	//intは32ビット、unsigned shordは16ビット
+	//海のグリッド程度ならばそこまで頂点数は多くならないので、
+	//unsigned shortを使用する
+	std::vector<unsigned short> m_indices;
+
+	//グリッドのサイズの頂点数分の頂点データを作成する
+	std::vector<VERTEX3D> m_vertexData;
+
+	//頂点バッファのハンドル
+	int m_vertexBufferH;
+	//インデックスバッファのハンドル
+	int m_indexBufferH;
 };
 
